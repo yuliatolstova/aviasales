1b57610c1863da92dad8eafc230e0b32a5140c0b
Юлия А. Толстова <tolstova@guim>
2023-04-13 10:35:40 +0700

    time

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -5,12 +5,23 @@ import { Card } from 'antd'
 import styles from './TicketCard.module.scss'
 
 export default function TicketCard({ price, airlines, segments }) {
+  const timeTravel = (data, duration) => {
+    let timeDeparture = new Date(data)
+    let timeArrivals = new Date(timeDeparture.getTime() + duration * 60 * 1000)
+    const formatTime = (date) => {
+      const hours = date.getHours().toString().padStart(2, '0')
+      const minutes = date.getMinutes().toString().padStart(2, '0')
+      return `${hours}:${minutes}`
+    }
+    return [formatTime(timeDeparture), formatTime(timeArrivals)]
+  }
   let info = segments.map((seg) => {
+    const [timeDeparture, timeArrivals] = timeTravel(seg.date, seg.duration)
     return (
       <div key={seg.origin + seg.destination + seg.duration} className={styles.ticket__info}>
         <div className={styles.ticket__way}>
           <span className={styles.ticket__title}>{`${seg.origin} - ${seg.destination}`}</span>
-          <span className={styles.ticket__subTitle}>11:20 – 00:50</span>
+          <span className={styles.ticket__subTitle}>{`${timeDeparture} - ${timeArrivals}`}</span>
         </div>
         <div className={styles.ticket__wayTime}>
           <span className={styles.ticket__title}>В пути</span>
